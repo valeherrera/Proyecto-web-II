54d14b5e837c35a75e27155beb8b460596ed2a1c
Valeria Herrera <37224240+valeherrera@users.noreply.github.com>
2019-05-23 12:58:59 -0600

    tipos usuarios

--- proyectoWeb2/src/app/app-routing.module.ts
+++ proyectoWeb2/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UsuarioComponent} from './usuario/usuario.component';
+import { AdminComponent} from './admin/admin.component';
+
 
 const routes: Routes = [
 
-  { path: '', component: LoginComponent },
-  { path: 'Login', component: LoginComponent }
-  
+  { path: '', component: LoginComponent},
+  { path: 'Login', component: LoginComponent},
+  { path: 'Usuario', component: UsuarioComponent},
+  { path: 'Admin', component: AdminComponent}
+
 ];
 
 @NgModule({
